e3f764bfa8b68e28f04ea940098613a310ad9f84
Guobin Zhao <53704568+sxm13@users.noreply.github.com>
2024-01-03 14:17:14 +0900

    Update steam-charge-web.py

--- steam-charge-web.py
+++ steam-charge-web.py
@@ -3,10 +3,14 @@ from predict import predict_with_model
 
 st.title('MOF/COF GCN Charges Predicter')
 uploaded_file = st.file_uploader("Please upload your CIF file", type="cif")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    with open('./upload.cif', 'wb') as f:
+        f.write(bytes_data)
 model_option = st.radio("Type", ('MOF', 'COF'))
 
 if uploaded_file is not None and model_option:
-    prediction = predict_with_model(model_option, uploaded_file)
+    prediction = predict_with_model(model_option, 'upload.cif')
     st.write("predicted result: ")
     st.write(prediction)
     st.download_button(label="Download cif file with charges", data=prediction, file_name="prediction.cif", mime="text/cif")
