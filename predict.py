04cda440eb469f9a70b560e49d04b837e6858640
Guobin Zhao <53704568+sxm13@users.noreply.github.com>
2024-01-03 13:44:32 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -15,9 +15,9 @@ def predict_with_model(model_name, file):
         model_ddec_name = "./pth/MOF/ddec.pth"
         ddec_nor_name = "./pth/MOF/normalizer-ddec.pkl"
     gcn = load_gcn(model_pbe_name)
-    with open(ddec_nor_name, 'rb') as f:
-        ddec_nor = pickle.load(f)
-    f.close()
+    # with open(ddec_nor_name, 'rb') as f:
+    #     ddec_nor = pickle.load(f)
+    # f.close()
     ase_format(file)
     CIF2json(file,save_path="")
     pre4pre(file,"","")
