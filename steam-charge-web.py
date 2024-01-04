7e15f47ee87f303fa6829a60852e57197a903a07
Guobin Zhao <53704568+sxm13@users.noreply.github.com>
2024-01-04 13:32:28 +0900

    Update steam-charge-web.py

--- steam-charge-web.py
+++ steam-charge-web.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from predict import predict_with_model
 
-st.title('MOF/COF GCN Charges Predicter')
+st.title('MOF/COF GCN Charges Predicter')                            
+st.markdown("Contact: sxmzhaogb@gmail.com")
+st.markdown(' :heart_eyes: <span style="color:grey;">Cite as: GCNCharges ****</span>', unsafe_allow_html=True)
+
 uploaded_file = st.file_uploader("Please upload your CIF file", type="cif")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
@@ -16,3 +19,5 @@ if uploaded_file is not None and model_option:
         st.download_button(label="Download cif file with charges", data=prediction, file_name="prediction.cif")
     else:
         st.write("No data available for download")
+
+st.markdown('<span style="color:grey;">Site developed and maintained by Guobin Zhao (Pusan National University)</span>', unsafe_allow_html=True)
