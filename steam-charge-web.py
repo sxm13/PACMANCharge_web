0242094e198ed5cb7d4338024c7a42d5941954be
Guobin Zhao <53704568+sxm13@users.noreply.github.com>
2024-01-06 12:55:47 +0900

    Update steam-charge-web.py

--- steam-charge-web.py
+++ steam-charge-web.py
@@ -1,22 +1,27 @@
 import streamlit as st
 from predict import predict_with_model
 
-st.title('MOF/COF GCN Charges Predicter')                            
+st.title('MOF/COF GCN Charges Predictor')
 st.markdown("Contact: sxmzhaogb@gmail.com")
 st.markdown(' :heart_eyes: <span style="color:grey;">Cite as: GCNCharges ****</span>', unsafe_allow_html=True)
 
 uploaded_file = st.file_uploader("Please upload your CIF file", type="cif")
 if uploaded_file is not None:
+    # Extract the file name without extension
+    file_name = uploaded_file.name.split('.')[0]
+
     bytes_data = uploaded_file.getvalue()
-    with open('./upload.cif', 'wb') as f:
+    with open(f'./{file_name}.cif', 'wb') as f:
         f.write(bytes_data)
+
 model_option = st.radio("Type", ('MOF', 'COF'))
 
 if uploaded_file is not None and model_option:
-    prediction = predict_with_model(model_option, 'upload.cif')
+    prediction = predict_with_model(model_option, f'{file_name}.cif')
     st.write("predicting")
     if prediction is not None:
-        st.download_button(label="Download cif file with charges", data=prediction, file_name="prediction.cif")
+        # Use the input file name for the output file
+        st.download_button(label="Download cif file with charges", data=prediction, file_name=f"{file_name}_prediction.cif")
     else:
         st.write("No data available for download")
 
