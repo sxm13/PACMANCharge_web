1b0293d4fe5e6989fb2dae54883544dc1970c151
Guobin Zhao <53704568+sxm13@users.noreply.github.com>
2024-01-31 00:23:37 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,6 +5,7 @@ import numpy as np
 from model4pre.GCN_ddec import SemiFullGN
 from model4pre.data import collate_pool, get_data_loader, CIFData, load_gcn
 from model4pre.cif2data import ase_format, CIF2json, pre4pre, write4cif
+import importlib
 
 source = importlib.import_module('model4pre')
 sys.modules['source'] = source
