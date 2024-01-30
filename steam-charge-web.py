758e1eae8c218bf9a4d2146c21fcab77b9b14e6b
Guobin Zhao <53704568+sxm13@users.noreply.github.com>
2024-01-31 01:14:54 +0900

    Update steam-charge-web.py

--- steam-charge-web.py
+++ steam-charge-web.py
@@ -7,9 +7,7 @@ st.markdown(' :heart_eyes: <span style="color:grey;">Cite as: GCNCharges ****</s
 
 uploaded_file = st.file_uploader("Please upload your CIF file", type="cif")
 if uploaded_file is not None:
-    # Extract the file name without extension
     file_name = uploaded_file.name.split('.')[0]
-
     bytes_data = uploaded_file.getvalue()
     with open(f'./{file_name}.cif', 'wb') as f:
         f.write(bytes_data)
@@ -17,12 +15,11 @@ if uploaded_file is not None:
 model_option = st.radio("Type", ('MOF', 'COF'))
 
 if uploaded_file is not None and model_option:
-    prediction = predict_with_model(model_option, f'{file_name}.cif')
+    prediction = predict_with_model(model_option, f'{file_name}.cif',file_name)
     st.write("please download structure with GCN Charge")
-    if prediction is not None:
-        # Use the input file name for the output file
-        st.download_button(label="Download cif file with charges", data=prediction, file_name=f"{file_name}_gcn.cif")
-    else:
-        st.write("No data available for download")
+    # if prediction is not None:
+    st.download_button(label="Download cif file with charges", data=prediction, file_name=f"{file_name}_gcn.cif")
+    # else:
+    #     st.write("No data available for download")
 
 st.markdown('<span style="color:grey;">Site developed and maintained by Guobin Zhao (Pusan National University)</span>', unsafe_allow_html=True)
